02abe4bc2a95b40ea196b74bb3843cfcebd918ed
Pedro Manrique <232200100>
2026-06-07 18:13:29 -0500

    feat: comentario de prueba en CitaController

--- SDGP_VVP.api/Controllers/CitaController.cs
+++ SDGP_VVP.api/Controllers/CitaController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SDGP_VVP.Core.DTOs;
 using SDGP_VVP.Infrastructure.Data;
-// prueba github    
+// Cambio de prueba para GitHub    
 namespace SDGP_VVP.api.Controllers
 {
     [Route("api/[controller]")]
